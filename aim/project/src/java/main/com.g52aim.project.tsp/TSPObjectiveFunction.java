4910a2641012273f167e9514684bc7330b3bc382
williamvds <w.vigolodasilva@gmail.com>
2018-04-05 14:08:06 +0100

    aim project: objective function

--- aim/project/src/java/main/com.g52aim.project.tsp/TSPObjectiveFunction.java
+++ aim/project/src/java/main/com.g52aim.project.tsp/TSPObjectiveFunction.java
@@ -1,38 +1,42 @@
 package com.g52aim.project.tsp;
 
+import com.g52aim.project.tsp.instance.Location;
 import com.g52aim.project.tsp.interfaces.ObjectiveFunctionInterface;
 import com.g52aim.project.tsp.interfaces.TSPInstanceInterface;
 import com.g52aim.project.tsp.solution.SolutionRepresentation;
 
 /**
- * 
+ *
  * @author Warren G. Jackson
  *
  */
 public class TSPObjectiveFunction implements ObjectiveFunctionInterface {
-	
+
 	private final TSPInstanceInterface instance;
-	
+
 	public TSPObjectiveFunction(TSPInstanceInterface instance) {
-		
+
 		this.instance = instance;
 	}
 
 	@Override
 	public double getObjectiveFunctionValue(SolutionRepresentation solution) {
-		
-		double cost = 0.0;
+    double total = 0;
+    for (int i = 0; i < solution.getNumberOfCities() -1; i++) {
+      total += getCost(i, i +1);
+    }
 
-		// TODO calculate cost of entire solution
-		// you should complete this regardless of whether
-		// you are going to be using the basic or delta evaluation
 		return cost;
 	}
 
 	@Override
 	public double getCost(int location_a, int location_b) {
-		
-		return 0.0;
+		Location a = instance.getLocationForCity(location_a),
+		         b = instance.getLocationForCity(location_b);
+
+    // Euclidean distance
+    return Math.round(
+      Math.sqrt((a.getX() - b.getX())^2 + (a.getY() - b.getY())^2));
 	}
 
 }
