531004870f1b93477fd7c5e9f9baf6870d85ec22
williamvds <w.vigolodasilva@gmail.com>
2018-05-02 18:36:49 +0100

    aim project: Reinsertion: delta eval is fine

--- aim/project/src/java/main/com.g52aim.project.tsp/heuristics/Reinsertion.java
+++ aim/project/src/java/main/com.g52aim.project.tsp/heuristics/Reinsertion.java
@@ -33,17 +33,16 @@ public class Reinsertion extends HeuristicOperators implements HeuristicInterfac
 			int a = random.nextInt(cities),
 					b = -1, temp;
 
-			// TODO fix delta eval
 			// subtract distance before and after removed city
-			// value -= f.getCost(repr.get(a), repr.get(Math.floorMod(a -1, cities)));
-			// value -= f.getCost(repr.get(a), repr.get((a +1) % cities));
+			value -= f.getCost(repr.get(a), repr.get(Math.floorMod(a -1, cities)));
+			value -= f.getCost(repr.get(a), repr.get((a +1) % cities));
 
 			do {
 				b = random.nextInt(cities);
 			} while (a == b);
 
 			// subtract distance between city at insertion point and previous
-			// value -= f.getCost(repr.get(b), repr.get(Math.floorMod(b -1, cities)));
+			value -= f.getCost(repr.get(b), repr.get(Math.floorMod(b -1, cities)));
 
 			// reinsert
 			temp = repr.get(a);
@@ -51,13 +50,12 @@ public class Reinsertion extends HeuristicOperators implements HeuristicInterfac
 			repr.add(b, temp);
 
 			// add distance between new city and previous, and next city
-			// value += f.getCost(repr.get(b), repr.get(Math.floorMod(b -1, cities)));
-			// value += f.getCost(repr.get(b), repr.get((b +1) % cities));
+			value += f.getCost(repr.get(b), repr.get(Math.floorMod(b -1, cities)));
+			value += f.getCost(repr.get(b), repr.get((b +1) % cities));
 		}
 
 		solution.getSolutionRepresentation().setRepresentationOfSolution(
 			repr.toArray(array));
-		value = f.getObjectiveFunctionValue(solution.getSolutionRepresentation());
 		solution.setObjectiveFunctionValue(value);
 		return value;
 	}
